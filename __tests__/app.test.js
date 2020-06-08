0aaf677d9c739b7f014c612b0148ad7f591bdd15
Lucia Brammer <brammerl96@gmail.com>
2020-06-08 15:00:08 -0700

    finished end points and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,44 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it(`handles / route`, () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual(`hi`);
+      });
+  });
 
+  it(`handles /echo route`, () => {
+    return request(app)
+      .post(`/echo`)
+      .send('hello')
+      .then(res => {
+        expect(res.text).toEqual('hello');
+      });
+  });
+
+  it(`handles /red route`, () => {
+    return request(app)
+      .get(`/red`)
+      .then(res => {
+        expect(res.text).toEqual(`<h1>red</h1>`);
+      });
+  });
+
+  it(`handles /green route`, () => {
+    return request(app)
+      .get(`/green`)
+      .then(res => {
+        expect(res.text).toEqual(`<h1>green</h1>`);
+      });
+  });
+
+  it(`handles /blue route`, () => {
+    return request(app)
+      .get(`/blue`)
+      .then(res => {
+        expect(res.text).toEqual(`<h1>blue</h1>`);
+      });
+  });
 });
